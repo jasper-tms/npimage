567a2ecec44323f8f6b9e34ff073ab264eb8c79a
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2021-07-22 23:06:17 -0400

    Implemented an 'offset' function to move images.
    Currently integer offsets only. Subpixel offsets is a TODO.

--- npimage/operations.py
+++ npimage/operations.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
+
 import numpy as np
+
+
+def offset(image, distance, fill_value=0):
+    """
+    Offset an image by a given distance. distance must be an iterable with
+    length matching the number of axes in the image, to specify an offset along
+    eaceh axis.
+    The pixels no longer occupied by the original image will be filled in with
+    fill_value.
+    """
+    if len(image.shape) != len(distance):
+        m = (f'distance must have length {len(image.shape)} to specify an'
+             ' offset along each axis of the image, but instead had length'
+             f' {len(distance)}')
+        raise ValueError(m)
+    # TODO implement fractional offsets
+    if any([abs(x - int(x)) > 1e-8 for x in distance]):
+        print('WARNING: fractional offsets not supported, will truncate.')
+    distance = [int(x) for x in distance]
+
+    new_image = np.full_like(image, fill_value)  # Blank image filled with fill_value
+    source_range = [slice(max(0, -d), min(s, s-d)) for d, s in zip(distance, image.shape)]
+    target_range = [slice(max(0, d), min(s, s+d)) for d, s in zip(distance, image.shape)]
+
+    new_image[tuple(target_range)] = image[tuple(source_range)]
+
+    return new_image
+
 
 def remove_bleedthrough(im, contaminated_slice, source_slice,
                         leave_saturated_pixels_alone=True):
