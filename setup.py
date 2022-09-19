4bc0f312e415d23f294a955c77b3c91614e16a71
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-09-19 10:30:38 +0200

    Bump version 1.0.0 -> 1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="numpyimage",
-    version="1.0.0",
+    version="1.0.1",
     author="Jasper Phelps",
     author_email="jasper.s.phelps@gmail.com",
     description="Load, save, and manipulate image files as numpy arrays",
