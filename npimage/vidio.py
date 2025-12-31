c7a28ca25e23d58e11e0669bcc07681f1b8cb4d1
Jasper Phelps <jasper.s.phelps@gmail.com>
2025-12-31 17:26:03 +0100

    Make `with npimage.VideoStreamer(...)` work by adding `enter` & `exit`

--- npimage/vidio.py
+++ npimage/vidio.py
@@ -512,6 +512,12 @@ class VideoStreamer:
         except Exception:
             pass
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
 
 class AVVideoWriter:
     """
